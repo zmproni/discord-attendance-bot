f5c2c111958de429591aa09292babe41caffac7a
Alessandro Zamproni <zmproni@gmail.com>
2021-01-02 23:02:54 +0800

    Added two commands

---
+++ src/commands/amend.js
@@ -0,0 +1,24 @@
+const { Message } = require("discord.js");
+const Config = require("../util/config");
+/**
+ * @typedef {{ import('../structure/command').Command }} Command
+ */
+const command = "amend"
+/** @type{Command} */
+module.exports = {
+  name: command,
+  description: "Allows user to ",
+  usage: `${Config.prefix}${command} <time> <note>
+  Eg: ${Config.prefix}attend 10:00am HQ
+  <time> -> Time clocking in in HH:mmtt format, enter "-" for current time.
+  <note> -> Additional information to append to attendance.
+  `,
+  requireAdminRights: false,
+  /**
+   * @param {Message} message
+   * @param {Array.<string>} args
+   */
+  async execute(message, args) {
+    message.channel.send("Message received");
+  }
+}
