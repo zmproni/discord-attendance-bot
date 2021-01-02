43bfad02e9836fad693edf5b59fdf1d787cfe2d9
Alessandro Zamproni <zmproni@gmail.com>
2021-01-02 17:35:50 +0800

    Typedef for args

--- src/commands/nickname.js
+++ src/commands/nickname.js
@@ -1,11 +1,15 @@
 const { Message } = require("discord.js");
 
+/**
+ * @typedef {{channelId: number, commandPrefix: string}} Args
+ */
+
 module.exports = {
     name: "nickname",
     description: "Changes the user's nickname in the server.",
     /**
      * @param {Message} message
-     * @param {string} args
+     * @param {Args} args
      */
     async execute(message, args) {
         /** @type {string?} */
