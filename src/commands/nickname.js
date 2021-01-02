8ef5e5a96e297cd8ba4653283a1bbeeb36873ede
Alessandro Zamproni <zmproni@gmail.com>
2021-01-02 22:40:54 +0800

    Added usage field

--- src/commands/nickname.js
+++ src/commands/nickname.js
@@ -1,5 +1,5 @@
 const { Message } = require("discord.js");
-
+const Config = require("../util/config");
 /**
  * @typedef {{ import('../structure/command.js').Command }} Command
  */
@@ -8,13 +8,13 @@ const { Message } = require("discord.js");
 module.exports = {
     name: "nickname",
     description: "Changes the user's nickname in the server.",
+    usage: `${Config.prefix}nickname <Your new nickname>`,
     requireAdminRights: false,
     /**
      * @param {Message} message
      * @param {Array.<string>} args
      */
     async execute(message, args) {
-        console.log("executed")
         message.channel.send("Message received");
    }
 }
--- src/structure/command.js
+++ src/structure/command.js
@@ -2,6 +2,7 @@
  * @typedef {Object} Command
  * @property {string} name
  * @property {string} description
+ * @property {string} usage
  * @property {boolean} requireAdminRights
  * @property {Function} execute
  */
