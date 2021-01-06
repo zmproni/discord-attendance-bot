675f289085fa29120d8517e107e2d464038f96f3
iamanandri <irfananandri@gmail.com>
2021-01-06 15:11:45 +0700

    adding Attendance.js and Session.js classes

---
+++ src/structure/Attendance.js
@@ -0,0 +1,14 @@
+class Attendance {
+
+    constructor() {
+        this.username;
+        this.nickname;
+        this.time;
+        this.sessionId;
+        this.note;
+        this.type;
+    }
+
+}
+
+module.exports = Attendance;
