b27b23c63e8a5e2c421f3e37ddffd713e1da9074
Alessandro Zamproni <zmproni@gmail.com>
2021-01-07 16:04:11 +0800

    Added method def for session

--- src/structure/Session.js
+++ src/structure/Session.js
@@ -1,17 +1,32 @@
 let date = new Date();
 
 class Session {
+
     constructor(name, startDateTime, duration) {
-        this.id; // import uid generator and auto generate on object creation
+        this._id; // import uid generator and auto generate on object creation
+        this._startDateTime = startDateTime;
+        this._attendanceList = [];
         this.name = name;
-        this.startDateTime = startDateTime;
         this.duration = duration ; // Figure out what data struct to use
-        this.attendanceList = [];
     }
 
-    isActive() {
+    addAttendance() {}
+    editAttendace() {}
+    fetchAttendance() {}
+    isActive() { }
 
+    get id() {
+        return this._id;
     }
+
+    get attendanceList() {
+        return this._attendanceList;
+    }
+
+    get startDateTime() {
+        return this._startDateTime;
+    }
+
 }
 
 module.exports = Session;
